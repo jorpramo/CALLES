49c9782c9f6cb5152fa15f1332d30c412db8103d
Jorge <jorpramo@gmail.com>
2017-02-13 19:11:18 +0100

    m

--- busqueda.py
+++ busqueda.py
@@ -3,9 +3,10 @@
 
 __author__ = 'jpradas'
 import wikipedia
-import settings as set
+import settings
+from collections import Counter
 from fastkml import kml
-import csv, re
+import csv, re, os
 
 
 def procesa_KML(fichero):
@@ -22,85 +23,104 @@ def procesa_KML(fichero):
         print(list_detallado[i].name)
 
 categorias=[]
-for k, v in set.categorias.items():
+for k, v in settings.categorias.items():
     categorias.append(v)
 categorias='|'.join(categorias)
 
-
-def listaunica(seq):
-   # not order preserving
-   set = {}
-   map(set.__setitem__, seq, [])
-   return set.keys()
-
-
 def categoriza(pagina):
-    print(pagina)
+    #print(pagina)
     result=re.findall(categorias,pagina)
     return result
 
 def procesa_csv(fichero):
+    try:
+        os.remove('sincategorizar.csv')
+    except:
+        pass
     wikipedia.set_lang("es")
     i=0
-    #temp = open(fichero+'_tmp'.csv, 'wb')
+    total_cat=0
+    total_cat_total=0
+    total=0
+    sin = open('sin.csv','w', newline='')
+    temp = open('tmp.csv','w', newline='')
+    writer = csv.writer(temp, delimiter=';')
+    writer_sin = csv.writer(sin, delimiter=';')
+    texto=""
     with open(fichero, newline='') as csvfile:
-
 
         spamreader = csv.reader(csvfile, delimiter=';')
         for row in spamreader:
-            i=i+1
-            if (i>6):
-                break
-            print(row[3])
-            result=wikipedia.search(row[3])
-            categoria=[]
-            for cad in result[:3]:
-                try:
-                    pag=wikipedia.page(cad)
-                    #head = list(islice(pag.content, 10))
-                    cat_temp=categoriza(pag.content[:200].lower())
-                    if cat_temp!='':
-                        categoria.append(cat_temp)
-                except:
-                    pass
-            cat=[]
-            categoria=[item for sublist in categoria for item in sublist]
-            print(categoria)
-            if len(categoria)>0:
-                for k, v in set.categorias.items():
-                    for c in categoria:
-                        if c in v:
-                            cat.append(k)
-            print(cat)
-            max=0
-            cat_final=''
-            if len(cat)>0:
-                for unica in list(set(cat)):
-                    cont=cat.count(unica)
-                    if cont>max:
-                        max=cont
-                        cat_final=unica
+            total=total+1
+            if (len(row)<=5) | (row[5]==''):
+                categoria=[]
+                #buscamos por nombre primero
+                cat_temp=categoriza(row[3].lower()
 
-            print(cat_final)
-            #writer.writerow(row[0], row[1],row[2],row[3],row[4], categoria)
+                                    )
+                if (1==1):#len(cat_temp)!=0:
+                    categoria.append(cat_temp)
+                else:
+                    result=wikipedia.search(row[3])
+                    print(row[3])
+
+                    for cad in result[:1]:
+                        try:
+                            pag=wikipedia.page(cad)
+                            cat_temp=categoriza(pag.content[:200].lower())
+                            if cat_temp!='':
+
+                                categoria.append(cat_temp)
+                        except:
+                            pass
+                cat=[]
+                categoria=[item for sublist in categoria for item in sublist]
+                if len(categoria)>0:
+                    for k, v in settings.categorias.items():
+                        for c in categoria:
+                            if c in v:
+                                cat.append(k)
+                max=0
+                cat_final=''
+                if len(cat)>0:
+                    total_cat = total_cat + 1
+                    for unica in set(cat):
+                        cont=cat.count(unica)
+                        if cont>max:
+                            max=cont
+                            cat_final=unica
+
+                writer.writerow([row[0], row[1],row[2],row[3],row[4],cat_final])
+                #texto.append(row[3].lower())
+                texto=texto +" "+  row[3].lower()
+            else:
+                total_cat_total = total_cat_total + 1
+                writer.writerow(row)
+
+    temp.close()
+    sin.close()
+    try:
+        os.remove('old.csv')
+    except:
+        pass
+
+    os.rename(fichero, 'old.csv')
+    os.rename('tmp.csv',fichero)
+    print("Total Categorizadas en esta ejecución",total_cat)
+    print("Total Categorizadas ",total_cat_total)
+    print("Total Procesadas ",total)
+    texto = re.sub(r'\b\w{1,3}\b', '', texto)
+    texto = re.findall(r'\w+', texto)
+
+    print(Counter(texto).most_common(100))
 
 
-
-ciudades=set.ciudades
+ciudades=settings.ciudades
 
 for c in ciudades:
     if c['tipo']=='KML':
         procesa_KML(c['fichero'])
 
     if c['tipo']=="CSV":
-        procesa_csv(c['fichero']
-                    )
-"""
-wikipedia.set_lang("es")
-result=wikipedia.search("Peset Aleixandre")
-
-for cad in result:
-    pag=wikipedia.page(cad)
-    print(pag.content)
-"""
+        procesa_csv(c['fichero'])
 
--- settings.py
+++ settings.py
@@ -17,9 +17,13 @@ ciudades.append({
 })
 
 
-categorias={'LETRAS':'escritor|periodista|filosofo|historiador',
-            'LUGAR':'barrio|población|ciudad|pueblo|país',
-            'CIENCIA':'médico|doctor|investigador|físico|químico',
-            'RELIGION':'cura|monja|santo|religioso',
-            'ARTE':'pintor|escultor|músico',
-            'POLITICA':'político|senador|abogado'}
+categorias={'LETRAS':'escritor|periodista|filósofo|historiador|poeta|escriptor|dramaturg|musico|maestro|profesor|cronista|escriptor|periodista|filòsof|historiador|poeta|escriptor|dramaturg|músic|mestre|professor|professor|cronista',
+            'CIENCIA':'médico|doctor|investigador|físico|químico|economista|metge|cardioleg|ingeniero|metge|doctor|investigador|físic|químic|economista|Metge|cardioleg|enginyer',
+            'RELIGION':'cura|monja|santo|religioso|arzobispo|hermandad|iglesia|parroquia|virgen|santa|san|padre|obispo|canonigo|reverendo|cardenal|cura|monja|sant|religiós|arquebisbe|germanor|església|parròquia|verge|santa|sant|pare|bisbe|canonge|reverend|cardenal',
+            'ARTE':'pintor|escultor|músico|arquitecto|actor|grabador|compositor|pintor|escultor|músic|arquitecte|actor|gravador|compositor|music',
+            'POLITICA':'político|senador|abogado|militante|presidente|ministro|republica|alcalde|rey|reina|polític|senador|advocat|militant|president|ministre|republica|alcalde|rei|reina',
+            'MILITAR':'teniente|coronel|comandante|soldado|militar|general|tinent|coronel|comandant|soldat|militar|general|',
+            'EVENTO':'enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre|fecha|gener|febrer|març|abril|maig|juny|juliol|agost|setembre|octubre|novembre|desembre|data',
+            'TITULO':'duque|duquesa|noble|conde|condesa|baron|baronesa|marques|señorío|duc|duquessa|noble|comte|comtessa|baron|baronessa|marquis|senyoriu',
+            'LUGAR':'municipio|ciudad|barrio|mercado|tallafoc|mar|rio|cauce|puente|casas|alqueria|casa|rambla|poblado|isla|azagador|camino|municipi|ciutat|barri|mercat|Tallafoc|mar|riu|llera|pont|cases|alqueria|casa|horta|platja|rambla|poblat|illa|assagador|camí'}
+
